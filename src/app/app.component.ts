c2e24cb077a68bb15591948912e27019035da1ab
katerina <sk.katherine@gmail.com>
2018-10-09 19:57:03 +0300

    some conditional styles done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
+export class AppComponent {
+  didSearch = false;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private route: ActivatedRoute) {
+    this.route.queryParamMap.subscribe(params => {
+      params.has("q") ? this.didSearch = true : this.didSearch = false;
+    });
+  }
 
   ngOnInit() {}
-  
+
   onSearch(name: string) {
+    this.didSearch = true;
     this.router.navigate(["/"], { queryParams: { q: name } });
+  }
+
+  clear() {
+    this.didSearch = false;
+    this.router.navigate(["/"]);
   }
 }
--- src/app/searchbox/searchbox.component.ts
+++ src/app/searchbox/searchbox.component.ts
@@ -8,7 +8,8 @@ import { Component, EventEmitter, Output, Input } from '@angular/core';
 export class SearchboxComponent {
 
   @Output() searched = new EventEmitter<string>();
- 
+  @Input() didSearch: boolean; 
+
   search(name: string) {
     this.searched.emit(name);
   }
