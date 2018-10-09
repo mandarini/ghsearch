cfec10557cea886a70c9b425dafe75f9f00c201f
katerina <sk.katherine@gmail.com>
2018-10-09 14:14:22 +0300

    deleted unnecessary files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,18 @@ import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchboxComponent } from './searchbox/searchbox.component';
-import { SearchComponent } from './search/search.component';
 
 import { RouterModule, Routes }  from '@angular/router';
-
-// import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchboxComponent,
-    SearchComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([])
-    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
